a075195eda9f218922f1f16c35efaa775e370ff5
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2024-02-08 14:46:08 +0100

    new version of Figure 8

--- figures/08_ALE_range.R
+++ figures/08_ALE_range.R
@@ -1,46 +1,44 @@
 library("stars")
 library("ggplot2")
 
-names = c("Jelenia Góra", "Katowice", "Kraków Zachodni", "Kutno",
-          "Nowy Targ", "Świnoujście", "Tomaszów Lubelski", "Toruń")
-names = as.factor(names)
-
 maps = list.files("data/raster_maps", pattern = "\\.tif$", full.names = TRUE)
 rds = list.files("code/ALE_test/rds", pattern = "\\.rds$", full.names = TRUE)
-output = data.frame(class = factor(), var = factor(), y = double(), Freq = integer())
 
-for (i in seq_along(names)) {
+df_output = data.frame(x = double(), y = double(), var = factor(), class = double(),
+                       class2 = factor())
+freq_output = data.frame(class = factor(), Freq = integer())
+
+for (i in seq_along(maps)) {
 
   ## class frequency
   class = read_stars(maps[i], proxy = FALSE)
   class[[1]] = as.integer(class[[1]])
   class = as.vector(class[[1]])
   freq = as.data.frame(table(class))
-  freq$name = names[i]
+  freq_output = rbind(freq_output, freq)
 
   ## ALE range
   df = readRDS(rds[i])
-  rang = aggregate(y ~ var + class2, df, FUN = \(x) max(x) - min(x))
-  rang = merge(rang, freq, by.x = "class2", by.y = "class")
-  colnames(rang)[1] = "class"
-
-  output = rbind(output, rang)
+  df_output = rbind(df_output, df)
 
 }
 
-## remove outlier in Katowice
-idx = which(output$name == "Katowice" & output$Freq == 6)
-output = output[-idx, ]
+freq_output = aggregate(Freq ~ class, freq_output, FUN = sum)
+df_output = aggregate(y ~ var + class2, df_output, FUN = \(x) max(x) - min(x))
+df_output = merge(df_output, freq_output, by.x = "class2", by.y = "class")
+size = sum(freq_output$Freq) # total area
+df_output$km_sq = df_output$Freq * 30 * 30 / 1000 / 1000 # convert to km^2
 
 
-ggplot(output, aes(log(Freq), y)) +
-  geom_point(aes(colour = var), alpha = 0.8, stroke = 0) +
-  geom_line(stat = "smooth", method = "loess", formula = "y ~ x", color = "black", alpha = 0.8) +
-  facet_wrap(vars(name), scales = "free_x") +
+ggplot(df_output, aes(Freq / size * 100, y)) +
+  geom_point(aes(colour = var), alpha = 0.8, stroke = 0, size = 2) +
+  geom_vline(xintercept = 0.5, linetype = "dashed", color = "darkgrey") +
+  scale_x_continuous(breaks = c(0.01, 0.1, 0.5, 1, 5, 10, 20, 30),
+                     labels = c(0.01, 0.1, 0.5, 1, 5, 10, 20, 30)) +
+  coord_trans(x = "log") +
   scale_colour_brewer(palette = "Dark2", name = "Variable") +
-  ylim(c(0, 2)) +
-  xlab("Landform representativeness (log)") +
-  ylab("Probability amplitude") +
+  xlab(bquote(bold("Area covered by the landform [%]"))) +
+  ylab("Amplitude of the impact change") +
   theme_bw() +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -48,6 +46,7 @@ ggplot(output, aes(log(Freq), y)) +
         axis.line = element_line(colour = "black", linewidth = 0.5),
         axis.title = element_text(face = "bold"),
         axis.text.x = element_text(angle = 45, vjust = 0.5),
-        strip.background = element_rect(fill = NA, colour = NA))
+        strip.background = element_rect(fill = NA, colour = NA)) +
+  guides(colour = guide_legend(override.aes = list(size = 2.5)))
 
-ggsave("figures/08_ALE_range.png", width = 8, height = 5)
+ggsave("08_ALE_range.png", width = 8, height = 5)
